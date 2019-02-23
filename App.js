e849d01d66fdf6d52d6310352ce0a295ab298a50
Kittaka <jdavisson87@gmail.com>
2019-02-22 20:47:38 -0600

    Feat: store
    Created a redux store on the main page of the app.

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
+import NewDeck from './components/NewDeck'
+import DeckList from './components/DeckList'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducer, middleware)}>
+        <View style={{flex:1}}>
+          <DeckList />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
