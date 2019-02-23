cb62eddbe85b95f041396a996cd9e0e14cb80f47
Kittaka <jdavisson87@gmail.com>
2019-02-22 20:54:35 -0600

    Feat: DeckList component shows Decks
    Added a feature to the deck list component to show all decks that are
    currently in the users AsyncStorage

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Platform } from 'react-native'
 import { fetchDecks } from '../utils/api'
+import { receiveDecks } from '../actions/decks'
+import { connect } from 'react-redux'
 
 class DeckList extends Component {
-  state= {
-    decks: {}
-  }
+
   componentDidMount(){
+    const { dispatch } = this.props
     fetchDecks()
-      .then((entries)=>this.setState({decks: JSON.parse(entries)}))
-      .catch((e)=>console.log('error ', e))
-      .then(()=>{
-        const { decks } = this.state
-        console.log(Object.keys(decks))
-      })
+      .then((decks)=>dispatch(receiveDecks(JSON.parse(decks))))
+      .then(()=>console.log('props', this.props))
   }
 
   render() {
-    const { decks } = this.state
+    const { decks } = this.props.decks
+
     return(
-      <View>
-      <Text>DeckList</Text>
-        {Object.keys(decks).map(deck=>{
-          console.log(decks[deck])
-          return(
-            <Text key={deck}>{decks[deck].title}</Text>
-          )
-        })}
+      <View style={styles.container}>
+        <View style={styles.center}>
+          <Text style={{fontSize:34}}>DeckList</Text>
+          {Object.keys(decks).map(deck=>{
+            return(
+              <Text key={deck}>{decks[deck].title}</Text>
+            )
+          })}
+        </View>
       </View>
     )
   }
 }
 
-export default DeckList
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    padding: 15
+  },
+  center: {
+    flex: 1,
+    //justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  }
+})
+
+function mapStateToProps (decks) {
+  return{
+    decks
+  }
+}
+
+
+export default connect(mapStateToProps)(DeckList)
