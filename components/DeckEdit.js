ccf772a80a6dd93523c627e85bca87bb3105dfb9
Kittaka <jdavisson87@gmail.com>
2019-03-04 23:33:56 -0600

    Fix: Questions Scrollable
    Put a ScrollView inside a View and it wouldn't let us scroll through
    the questions.  Removed the view, and now ScrollView works

--- components/DeckEdit.js
+++ components/DeckEdit.js
@@ -10,6 +10,7 @@ function WithCards({deck}) {
   return(
     <ScrollView>
       {deck.questions.map(card=>{
+        console.log(card.question)
           return(
             <Questions key={card.question} card={card}/>
           )
@@ -28,13 +29,13 @@ function NoCards(){
 
 class DeckEdit extends Component {
 
-  state={
-    questionLength: 0
-  }
+  // state={
+  //   questionLength: 0
+  // }
 
   remove = () => {
     const {deck, goBack, remove} = this.props
-    this.setState({questionLength: 0})
+    //this.setState({questionLength: 0})
     remove()
     goBack()
     removeDeck(deck)
@@ -47,50 +48,55 @@ class DeckEdit extends Component {
     }))
   }
 
-  componentWillMount() {
-    const {deck, decks} = this.props
-    const questionLength = decks.decks[deck].questions.length
-    this.setState({questionLength})
-  }
+  // componentWillMount() {
+  //   const { list, deck } = this.props
+  //   const questionLength = list[deck].questions.length
+  //   this.setState({questionLength})
+  // }
 
   render(){
-    const { decks, deck,  } = this.props
-    const { questionLength } =this.state
+    const { list, deck } = this.props
+    // this.setState({questionLength: list[deck].questions.length})
+    const questions = list[deck].questions
 
-
-    return(
-      <View>
-        <View>
-          <Text style={styles.deckHeader}>DeckEdit</Text>
-          <Text style={styles.deckName}>{deck}</Text>
-          <Text style={styles.deckHeader}>{questionLength} cards</Text>
-        </View>
-        <View style={styles.btnContainer}>
-          <TouchableOpacity style={styles.buttons} onPress={() => this.props.navigation.navigate(
-            'AddCard',
-            { deck }
-          )}>
-            <Text>Add Cards</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttons}>
-            <Text>Start Quiz</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttons} onPress={this.remove}>
-            <Text>Remove Deck</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          {questionLength>0
-            ? <WithCards deck={decks.decks[deck]}/>
+    console.log('😈', questions)
+      return(
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.deckHeader}>DeckEdit</Text>
+            <Text style={styles.deckName}>{deck}</Text>
+            <Text style={styles.deckHeader}>{questions.length} cards</Text>
+          </View>
+          <View style={styles.btnContainer}>
+            <TouchableOpacity style={styles.buttons} onPress={() => this.props.navigation.navigate(
+              'AddCard',
+              { deck }
+            )}>
+              <Text>Add Cards</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttons}>
+              <Text>Start Quiz</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttons} onPress={this.remove}>
+              <Text>Remove Deck</Text>
+            </TouchableOpacity>
+          </View>
+          {questions.length>0
+            ? <WithCards deck={list[deck]}/>
             : <NoCards/>
           }
         </View>
-      </View>
-    )
+      )
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 15,
+    alignItems: 'stretch',
+  },
   deckHeader:{
     fontSize: 28,
     fontWeight: 'bold',
@@ -125,12 +131,14 @@ const styles = StyleSheet.create({
   }
 })
 
-function mapStateToProps (decks, {navigation}) {
-  const deck= navigation.state.params.deck.title
+function mapStateToProps (state, {navigation}) {
+  const list = state.decks
+  const deck = navigation.state.params.deck.title
+  //const { deck }= navigation.state.params.deck.title
   return{
     deck,
-    decks,
-
+    list,
+    state
   }
 }
 
